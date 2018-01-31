455b3040c7b9406a1e322e8ce8890d664df7757d
zentim <zentim3248@gmail.com>
2018-01-31 18:36:53 +0800

    first commit

---
+++ app/components/Gallery.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+class Gallary extends Component {
+  render() {
+    let alternate = 'https://cdn0.iconfinder.com/data/icons/thin-photography/57/thin-367_photo_image_wall_unavailable_missing-512.png';
+
+    return (
+      <div>
+        {
+          this.props.items.map((item, index) => {
+            let {title, imageLinks, infoLink} = item.volumeInfo;
+            return (
+              <a
+                key={index}
+                className="book"
+                href={infoLink}
+                target="_blank"
+              >
+                <img
+                  src={imageLinks !== undefined ? imageLinks.thumbnail : alternate}
+                  alt="book image"
+                  className="book-img"
+                />
+                <div className="book-text">
+                  {title}
+                </div>
+              </a>
+            )
+          })
+        }
+      </div>
+    )
+  }
+}
+
+export default Gallary;
